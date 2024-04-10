e8232ccd3447655e88f17cf55831914a1f6bc9cd
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-10 18:56:03 +0200

    Feat:Create Product Card component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
 import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
@@ -10,9 +11,9 @@ export default function Home() {
         <div className="p-8">
           <HomeBanner />
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
-            return <div>{truncateText(product.name)}</div>
+            return <ProductCard data={product} />
           })}
         </div>
       </Container>
