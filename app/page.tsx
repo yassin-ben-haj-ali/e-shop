5710bffe39cd1d79e0f667753844f839a9c35cca
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-10 06:38:49 +0200

    Feat:Create products data and truncateText function

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { products } from "@/utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/truncateText";
 
 export default function Home() {
   return (
@@ -7,6 +9,11 @@ export default function Home() {
       <Container>
         <div className="p-8">
           <HomeBanner />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap:8">
+          {products.map((product: any) => {
+            return <div>{truncateText(product.name)}</div>
+          })}
         </div>
       </Container>
     </div>
