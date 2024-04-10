b0150566e95a717e0dc99a9336dfcf8cb5c3a0cd
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-10 03:22:14 +0200

    Fix:Improve footer UI and install react-icons

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,57 @@
+import Link from "next/link";
+import FooterList from "./FooterList";
+import Container from "../Container";
+import { MdFacebook } from "react-icons/md"
+import { AiFillInstagram, AiFillTwitterCircle, AiFillYoutube } from "react-icons/ai"
+
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+        <Container>
+            <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+                <FooterList>
+                    <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+                    <Link href="#">Phones</Link>
+                    <Link href="#">Laptops</Link>
+                    <Link href="#">Desktops</Link>
+                    <Link href="#">Watches</Link>
+                    <Link href="#">TVs</Link>
+                    <Link href="#">Accessories</Link>
+
+                </FooterList>
+                <FooterList>
+                    <h3 className="text-base font-bold mb-2">Customer Services</h3>
+                    <Link href="#">Contact us</Link>
+                    <Link href="#">Shipping Policy</Link>
+                    <Link href="#">Return & Exchanges</Link>
+                    <Link href="#">Watches</Link>
+                    <Link href="#">FAQs</Link>
+                </FooterList>
+                <div className="w-full md:w-1/3 mb-6 md:mb-0">
+                    <h3 className="text-base font-bold mb-2">About US</h3>
+                    <p className="mb-2">At our electronics store, we are dedicated to providing the latest and greatest devices and accessoires to our customers.With a wide selection of phones, Tvs, laptops, watches and accessories</p>
+                    <p>&copy; {new Date().getFullYear()} E-Shop. All rights reserved</p>
+                </div>
+                <FooterList>
+                    <h3 className="text-base font-bold mb-2">Follow US</h3>
+                    <div className="flex gap-2">
+                        <Link href="#">
+                            <MdFacebook size={24} />
+                        </Link>
+                        <Link href="#">
+                            <AiFillTwitterCircle size={24} />
+                        </Link>
+                        <Link href="#">
+                            <AiFillInstagram size={24} />
+                        </Link>
+                        <Link href="#">
+                            <AiFillYoutube size={24} />
+                        </Link>
+                    </div>
+                </FooterList>
+            </div>
+        </Container>
+    </footer>;
 }
- 
+
 export default Footer;
