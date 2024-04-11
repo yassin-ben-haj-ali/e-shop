27ed28057c508e1a45cb6f6c1759f0df99bef2bf
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-11 03:17:52 +0200

    Set up product details page

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -4,6 +4,7 @@ import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ProductCardProps {
@@ -11,10 +12,10 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-    const productRating = data.reviews.reduce((acc: number, item: any) =>
-        item.rating + acc, 0) / data.reviews.length;
+    const router = useRouter();
+    const productRating = data.reviews.reduce((acc: number, item: any) =>item.rating + acc, 0) / data.reviews.length;
 
-    return <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
+    return <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm" onClick={() => router.push(`/product/${data.id}`)}>
         <div className="flex flex-col items-center w-full gap-1">
             <div className="aspect-square overflow-hidden relative w-full">
                 <Image
---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,17 @@
+import Container from "@/app/components/Container";
+import ProductDetails from "./ProductDetails";
+import { product } from "@/utils/product";
+
+interface IParams {
+    productId: string
+}
+
+const Product = ({ params }: { params: IParams }) => {
+    return <div className="p-8">
+        <Container>
+            <ProductDetails product={product}/>
+        </Container>
+    </div>;
+}
+
+export default Product;
