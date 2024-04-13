a41ca679a90a348203f0520f7e641fc0535c2402
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-13 15:59:02 +0200

    Feat:Create Product Image component(display+select) product's image

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -5,6 +5,7 @@ import React, { useCallback, useState } from "react";
 import SetColor from "./SetColor";
 import SetQuantity from "./SetQuantity";
 import Button from "@/app/components/Button";
+import ProductImage from "./ProductImage";
 
 interface ProductDetailsProps {
     product: any
@@ -79,7 +80,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
     const productRating = product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) / product.reviews.length;
     return <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div>images</div>
+        <ProductImage
+            cartProduct={cartProduct}
+            product={product}
+            handleColorSelect={handleColorSelect}
+        />
         <div className="flex flex-col gap-1 text-slate-500 text-sm">
             <h2 className="text-3xl font-medium text-slate-700 ">{product.name}</h2>
             <div className="flex items-center gap-2">
