a51d5292312ae2df99f4274fc0981a983ee9963b
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-13 16:28:45 +0200

    install moment package+create Rating List of product

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/components/Container";
 import ProductDetails from "./ProductDetails";
 import { product } from "@/utils/product";
+import ListRating from "./ListRating";
 
 interface IParams {
     productId: string
@@ -9,7 +10,11 @@ interface IParams {
 const Product = ({ params }: { params: IParams }) => {
     return <div className="p-8">
         <Container>
-            <ProductDetails product={product}/>
+            <ProductDetails product={product} />
+            <div className="flex flex-col mt-20 gap-4">
+                <div>Add Rating</div>
+                <ListRating product={product} />
+            </div>
         </Container>
     </div>;
 }
