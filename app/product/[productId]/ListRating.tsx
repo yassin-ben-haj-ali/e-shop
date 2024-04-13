ba8de7bb6e2d4588ae60c128dbb0b1c32149f0f8
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-13 17:55:28 +0200

    Update Next.js configuration to allow images from additional domain+Create Avatar component

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Avatar from "@/app/components/Avatar";
 import Heading from "@/app/components/Heading";
 import { Rating } from "@mui/material";
 import moment from "moment";
@@ -19,7 +20,7 @@ const ListRating: React.FC<ListRatingProps> = ({
             {product.reviews && product.reviews.map((review: any) => {
                 return (<div key={review.id} className="max-w-300px">
                     <div className="flex gap-2 items-center">
-                        <div>Avatar</div>
+                        <Avatar src={review?.user.image} />
                         <div className="font-semibold">{review?.user.name}</div>
                         <div className="font-light">{moment(review.createdDate).fromNow()}</div>
                     </div>
