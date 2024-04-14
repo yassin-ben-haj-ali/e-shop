5325d1dc4556d801d6d86b84cb9714eb5160eaef
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-15 00:20:38 +0200

    Start Cart Page UI

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,13 @@
+"use client"
+import Container from "../components/Container";
+import CartClient from "./CartClient";
+
 const Cart = () => {
-    return (<div>Cart Page</div>);
+    return (<div className="pt-8">
+        <Container>
+            <CartClient />
+        </Container>
+    </div>);
 }
 
 export default Cart;
