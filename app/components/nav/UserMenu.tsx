4e0aeec688a7a122421fcba98a92bc7d108b1e04
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-16 14:32:46 +0200

    Feat:Create Backdrop component

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -6,32 +6,36 @@ import Avatar from "../Avatar";
 import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { signOut } from "next-auth/react"
+import BackDrop from "./BackDrop";
 
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleOpen = useCallback(() => {
         setIsOpen((prev) => !prev)
     }, [])
-    return (<div className="relative z-30">
-        <div onClick={toggleOpen} className="p-2 border-[1px] border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700">
-            <Avatar />
-            <AiFillCaretDown />
-        </div>
-        {isOpen && <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden top-12 right-0 text-sm flex flex-col cursor-pointer">
-            <div>
-                <Link href="/orders">
-                    <MenuItem onClick={toggleOpen}>Your orders</MenuItem>
-                </Link>
-                <Link href="/admin">
-                    <MenuItem onClick={toggleOpen}>Admin dashboard</MenuItem>
-                </Link>
-                <MenuItem onClick={() => {
-                    toggleOpen();
-                    signOut()
-                }}>logout</MenuItem>
+    return (<>
+        <div className="relative z-30">
+            <div onClick={toggleOpen} className="p-2 border-[1px] border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700">
+                <Avatar />
+                <AiFillCaretDown />
             </div>
-        </div>}
-    </div>);
+            {isOpen && <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden top-12 right-0 text-sm flex flex-col cursor-pointer">
+                <div>
+                    <Link href="/orders">
+                        <MenuItem onClick={toggleOpen}>Your orders</MenuItem>
+                    </Link>
+                    <Link href="/admin">
+                        <MenuItem onClick={toggleOpen}>Admin dashboard</MenuItem>
+                    </Link>
+                    <MenuItem onClick={() => {
+                        toggleOpen();
+                        signOut()
+                    }}>logout</MenuItem>
+                </div>
+            </div>}
+        </div>
+        {isOpen ? <BackDrop onClick={toggleOpen} /> : null}
+    </>)
 }
 
 export default UserMenu;
