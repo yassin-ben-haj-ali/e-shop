582905c71aa90a8069a869c52758e33a93533268
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-16 14:25:27 +0200

    Create UserMenu and MenuItem component

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const redressed = Redressed({ subsets: ['latin'], weight: ['400'] })
 const NavBar = () => {
@@ -13,7 +14,7 @@ const NavBar = () => {
                     <div className="hidden md:block">Search</div>
                     <div className="flex items-center gap-8 md:gap-12">
                         <CartCount />
-                        <div>UserMenu</div>
+                        <UserMenu />
                     </div>
                 </div>
             </Container>
