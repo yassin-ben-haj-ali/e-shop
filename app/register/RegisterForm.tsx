1ad41d3b3510d31026722672edfd46a8b976df51
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-16 12:30:29 +0200

    prisma configuration+register

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,8 +7,13 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
-
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation";
 const RegisterForm = () => {
+
+    const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
     const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
         defaultValues: {
@@ -18,14 +23,31 @@ const RegisterForm = () => {
         }
     })
 
+
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(false);
-        console.log(data);
+        axios.post('/api/register', data).then(() => {
+            toast.success("Account created")
+            signIn("credentials", {
+                email: data.email,
+                password: data.password,
+                redirect: false
+            }).then((callback) => {
+                if (callback?.ok) {
+                    router.push('/cart');
+                    router.refresh()
+                    toast.success('Logged In')
+                }
+                if (callback?.error) {
+                    toast.error(callback.error)
+                }
+            })
+        }).catch(() => toast.error("Something went wrong")).finally(() => setIsLoading(false))
     }
 
     return (<>
         <Heading title="Sign up for E-Shop" />
-        <Button outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={() =>{}} />
+        <Button outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={() => { }} />
         <hr className="bg-slate-300 w-full h-px" />
         <Input
             id="name"
