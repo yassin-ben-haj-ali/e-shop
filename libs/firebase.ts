ccbcea5351df69d45c8d41340e9ee715a2e1608e
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-22 09:27:56 +0200

    install and initialize firebase configuration

---
+++ libs/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions from the SDKs
+import { initializeApp } from "firebase/app";
+
+//Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgi9MTrAyb_miG1VCowtMRX0qqz2BxvJA",
+  authDomain: "e-shop-next-360db.firebaseapp.com",
+  projectId: "e-shop-next-360db",
+  storageBucket: "e-shop-next-360db.appspot.com",
+  messagingSenderId: "705243301487",
+  appId: "1:705243301487:web:9f7c63021492ca9070af9a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
