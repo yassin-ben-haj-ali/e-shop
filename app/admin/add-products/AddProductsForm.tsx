2bcae4e0dbefb1dbf3af40e5398fa9b447c1ae60
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-22 10:04:30 +0200

    Init Add Product Page

---
+++ app/admin/add-products/AddProductsForm.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/app/components/Heading";
+
+const AddProductsForm = () => {
+    return (<>
+        <Heading title="Add Product Form" />
+    </>);
+}
+
+export default AddProductsForm;
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,7 +1,23 @@
-const AddProducts = () => {
-    return ( <div>
+"use client"
+import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import AddProductsForm from "./AddProductsForm";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const AddProducts = async () => {
+    const currentUser = await getCurrentUser()
+    if (!currentUser || currentUser.role !== "ADMIN") {
+        return <NullData title="Oops! Access Denied" />
+    }
+    return (<div className="p-8">
+        <Container>
+            <FormWrap>
+                <AddProductsForm />
+            </FormWrap>
+        </Container>
         Add Products Page
-    </div> );
+    </div>);
 }
- 
+
 export default AddProducts;
