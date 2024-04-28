205aa605d3b2120f128d1b3728b9206075d133ff
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-04-28 14:35:01 +0200

    display order data and handle delivery Status (Server+Client)

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,5 +1,22 @@
-const ManageOrders = () => {
-    return (<div>Manage Orders Page</div>);
+import Container from "@/app/components/Container";
+import getOrders from "@/app/actions/getOrders";
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import ManageOrdersClient from "./ManageOrdersClient";
+
+const ManageOrders = async () => {
+    const orders = await getOrders();
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser || currentUser.role == "ADMIN") {
+        return <NullData title="Oops! Access Denied" />
+    }
+
+    return (<div className="pt-8">
+        <Container>
+            <ManageOrdersClient orders={orders} />
+        </Container>
+    </div>);
 }
 
 export default ManageOrders;
